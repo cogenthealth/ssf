4819f03f64fb2372a0f74a0df63744104e5a6274
Sujan Karki <regolithsjk@gmail.com>
2021-08-25 16:12:30 +0545

    ssf production url changed

--- src/config/api.php
+++ src/config/api.php
@@ -1,13 +1,13 @@
 <?php
 
     return [
-        "patient" => env("SSF_API_URL") . "/api_fhir_r4/Patient/",
-        "location" => env("SSF_API_URL") . "/api_fhir_r4/Location/",
-        "practitioner_role" => env("SSF_API_URL") . "/api_fhir_r4/PractitionerRole/",
-        "practitioner" => env("SSF_API_URL") . "/api_fhir_r4/Practitioner/",
-        "claim" => env("SSF_API_URL") . "/api_fhir_r4/claim/",
-        "claim_response" => env("SSF_API_URL") . "/api_fhir_r4/ClaimResponse/",
-        "communication_request" => env("SSF_API_URL") . "/api_fhir_r4/CommunicationRequest/",
-        "eligibility_request" => env("SSF_API_URL") . "/api_fhir_r4/CoverageEligibilityRequest/",
-        "coverage" => env("SSF_API_URL") . "/api_fhir_r4/Coverage/"
+        "patient" => env("SSF_API_URL") . "/api/api_fhir_r4/Patient/",
+        "location" => env("SSF_API_URL") . "/api/api_fhir_r4/Location/",
+        "practitioner_role" => env("SSF_API_URL") . "/api/api_fhir_r4/PractitionerRole/",
+        "practitioner" => env("SSF_API_URL") . "/api/api_fhir_r4/Practitioner/",
+        "claim" => env("SSF_API_URL") . "/api/api_fhir_r4/claim/",
+        "claim_response" => env("SSF_API_URL") . "/api/api_fhir_r4/ClaimResponse/",
+        "communication_request" => env("SSF_API_URL") . "/api/api_fhir_r4/CommunicationRequest/",
+        "eligibility_request" => env("SSF_API_URL") . "/api/api_fhir_r4/CoverageEligibilityRequest/",
+        "coverage" => env("SSF_API_URL") . "/api/api_fhir_r4/Coverage/"
     ];
